67f2299e2f6e3d36bfacd99eaa9f6822e19ee47c
sayaoyuri <yuriantunes0000@gmail.com>
2025-01-29 22:09:19 -0300

    chore(): fixing lint error for deploy & fixing header responsivity

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import LanguageToggle from "../languageToggle/LanguageToggle";
 
 export default function Header() {
   return (
-    <header className="w-full pt-6 pr-9 lg:pr-20 pl-7 flex justify-between">
+    <header className="w-full h-[150px] pt-6 pr-9 lg:pr-18 pl-7 flex justify-between">
       <Image className="w-28 md:w-36"
         src="/images/logo.svg"
         alt="sayaoyuri logo"
