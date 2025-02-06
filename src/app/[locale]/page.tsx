d9b560d705ae985fd7a5ea3c1f2e8c03f49ffd79
sayaoyuri <yuriantunes0000@gmail.com>
2025-02-06 20:02:22 -0300

    feat(about): implementing about section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header/Header";
 import Title from "@/components/title/Title";
+import About from "@/components/about/About";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Title /> 
+      <About />
     </>
   );
 }
