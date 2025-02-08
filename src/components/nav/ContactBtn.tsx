ce2e74240974a4889f31b2d0d77b0ed9b9c7d34c
sayaoyuri <yuriantunes0000@gmail.com>
2025-02-07 22:23:35 -0300

    fix(style): positioning Title image above background overlay and adjusting contact btn on hover

--- src/components/nav/ContactBtn.tsx
+++ src/components/nav/ContactBtn.tsx
@@ -5,7 +5,7 @@ export default function ContactBtn () {
   
   return (
     <button
-      className="hidden lg:flex justify-center items-center rounded-full w-48 h-10 text-black text-3xl bg-gradient-to-r from-[#0733FF] to-[#5505AB] hover:text-white hover:text-4xl"
+      className="hidden lg:flex justify-center items-center rounded-full w-48 h-10 text-black text-3xl bg-gradient-to-r from-[#0733FF] to-[#5505AB] hover:text-white"
     >
       {translation("contact")}
     </button>
